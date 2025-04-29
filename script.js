1fcdb6d6581f512cff88ea63b98eb6a29fdfe532
rafipan <rafi.panyotova@gmail.com>
2025-04-29 20:01:26 +0100

    Added backtracking

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 "use strict";
 
+let stack = [];
+let cells = undefined;
+let n = 4;
+
 document.addEventListener("DOMContentLoaded", () => {
     const maze = document.querySelector("#maze");
 
-    // initialise level counter and maze grid
-    let level = 1;
-    let n = 8;
-    drawGrid(maze, n);
-    const cells = maze.childNodes;
+    // draw maze grid
+    drawGrid(maze);
+    cells = maze.childNodes;
 
-    // create maze
+    // draw maze
     let current = cells[0]; // start at [i, j] = [0, 0]
-    visit(cells, current, n);
+    visit(current);
+
+    // initialise level counter
+    let level = 1;
+    let player = cells[0];
+    player.style["background-color"] = "#DC3C18";
+
+    let exit = cells[n * n - 1];
+    exit.style["background-color"] = "#DDDDDD";
 });
 
-function drawGrid(maze, n) {
+function drawGrid(maze) {
     maze.textContent = "";
     const cellWidth = (maze?.clientWidth) / n;
 
@@ -35,39 +45,29 @@ function drawGrid(maze, n) {
 }
 
 // recursively visit cells and create maze
-function visit(cells, cell, n) {
+function visit(cell) {
     // mark current cell as visited
     cell.dataset.visited = true;
-    cell.style["background-color"] = "#1789fc";/////////////////////////// just here for debugging
 
-    let neighbour = randomNeighbour(cells, cell, n);
+    // choose a random unvisted neighbour
+    let neighbour = randomNeighbour(cell); // = undefined if no such neighbours exist
     if ( neighbour ) {
-
         // remove border between current cell and neighbouring cell
-        if ( Number(neighbour.dataset.i) === Number(cell.dataset.i) - 1 &&
-             Number(neighbour.dataset.j) === Number(cell.dataset.j) ) { // top
-            cell.style["border-top"] = "none";
-            neighbour.style["border-bottom"] = "none";
-        } else if ( Number(neighbour.dataset.i) === Number(cell.dataset.i) &&
-                    Number(neighbour.dataset.j) === Number(cell.dataset.j) + 1 ) { // right
-            cell.style["border-right"] = "none";
-            neighbour.style["border-left"] = "none";
-        } else if ( Number(neighbour.dataset.i) === Number(cell.dataset.i) + 1 &&
-                    Number(neighbour.dataset.j) === Number(cell.dataset.j) ) { // bottom
-            cell.style["border-bottom"] = "none";
-            neighbour.style["border-top"] = "none";
-        } else { // left
-            cell.style["border-left"] = "none";
-            neighbour.style["border-right"] = "none";
-        }
+        removeWalls(cell, neighbour);
+
+        // push cell to stack
+        stack.push(cell);
 
         // recursively visit neighbour/next cell
-        visit(cells, neighbour, n);
+        visit(neighbour);
+    } else {
+        // pop from stack and visit recursively
+        if ( stack.length ) visit(stack.pop());
     }
 }
 
 // converts 2D coordinates to 1D index
-function index(i, j, n) {
+function index(i, j) {
     if ( i < 0 || j < 0 || i > n - 1 || j > n- 1 ) {
         return -1;
     }
@@ -75,7 +75,7 @@ function index(i, j, n) {
 }
 
 // returns a random unvisited neighbour
-function randomNeighbour(cells, cell, n) {
+function randomNeighbour(cell) {
     let indices = [ index(Number(cell.dataset.i) - 1, Number(cell.dataset.j)    , n),  // top
                     index(Number(cell.dataset.i),     Number(cell.dataset.j) + 1, n),  // right
                     index(Number(cell.dataset.i) + 1, Number(cell.dataset.j)    , n),  // bottom
@@ -92,4 +92,26 @@ function randomNeighbour(cells, cell, n) {
         return unvisited[Math.floor(Math.random() * unvisited.length)];
     }
     return undefined; // no unvisited neighbours to return
+}
+
+function removeWalls(cell, neighbour) {
+    if ( Number(neighbour.dataset.i) === Number(cell.dataset.i) - 1 &&
+         Number(neighbour.dataset.j) === Number(cell.dataset.j) ) { // top
+        cell.style["border-top"] = "none";
+        neighbour.style["border-bottom"] = "none";
+
+    } else if ( Number(neighbour.dataset.i) === Number(cell.dataset.i) &&
+                Number(neighbour.dataset.j) === Number(cell.dataset.j) + 1 ) { // right
+        cell.style["border-right"] = "none";
+        neighbour.style["border-left"] = "none";
+
+    } else if ( Number(neighbour.dataset.i) === Number(cell.dataset.i) + 1 &&
+                Number(neighbour.dataset.j) === Number(cell.dataset.j) ) { // bottom
+        cell.style["border-bottom"] = "none";
+        neighbour.style["border-top"] = "none";
+
+    } else { // left
+        cell.style["border-left"] = "none";
+        neighbour.style["border-right"] = "none";
+    }
 }
